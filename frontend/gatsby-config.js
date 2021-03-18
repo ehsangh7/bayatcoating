d7a8e30b6443c70453580316399ccfd9a0abfff4
ehsangh7 <ehsanghochagh@gmail.com>
2021-03-18 14:10:38 +0330

    add gatsby hosting config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
